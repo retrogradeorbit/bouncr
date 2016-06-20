9d58d2717d06bb31ff41ffd14bed47f49aad0ca4
Crispin Wellington <retrogradeorbit@gmail.com>
2016-06-20 23:02:40 +0800

    more cleanup

--- Bouncr.ino
+++ Bouncr.ino
@@ -277,45 +277,5 @@ void loop() {
 	buttons = lcd.readButtons();
       }
     }
-
-
-
-
   }
-
-
-
-
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  /* lcd.setCursor(0, 1); */
-  /* // print the number of seconds since reset: */
-  /* lcd.print(millis()/1000); */
-
-  /* uint8_t buttons = lcd.readButtons(); */
-
-  /* if (buttons) { */
-  /*   lcd.clear(); */
-  /*   lcd.setCursor(0,0); */
-  /*   if (buttons & BUTTON_UP) { */
-  /*     lcd.print("UP "); */
-  /*     lcd.setBacklight(1); */
-  /*   } */
-  /*   if (buttons & BUTTON_DOWN) { */
-  /*     lcd.print("DOWN "); */
-  /*     lcd.setBacklight(0); */
-  /*   } */
-  /*   if (buttons & BUTTON_LEFT) { */
-  /*     lcd.print("LEFT "); */
-  /*     lcd.setBacklight(0); */
-  /*   } */
-  /*   if (buttons & BUTTON_RIGHT) { */
-  /*     lcd.print("RIGHT "); */
-  /*     lcd.setBacklight(0); */
-  /*   } */
-  /*   if (buttons & BUTTON_SELECT) { */
-  /*     lcd.print("SELECT "); */
-  /*     lcd.setBacklight(0); */
-  /*   } */
-  /* } */
 }
