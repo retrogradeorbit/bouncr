97fb1ed1dc09190289406e703a41e3754d55c7fb
Crispin Wellington <retrogradeorbit@gmail.com>
2016-06-20 21:37:21 +0800

    clean up code

--- Bouncr.ino
+++ Bouncr.ino
@@ -1,16 +1,14 @@
-// Basic demo for accelerometer readings from Adafruit LIS3DH
+//
+// Bouncer Resonant Autorocker
+//
 
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_Sensor.h>
 #include <Stepper.h>
-
 #include <Adafruit_MCP23017.h>
 #include <Adafruit_RGBLCDShield.h>
-
-//#include <EEPROM.h>
-
 
 #include "window.h"
 #include "daccel.h"
@@ -20,46 +18,16 @@
 #define LIS3DH_CLK 13
 #define LIS3DH_MISO 12
 #define LIS3DH_MOSI 11
+
 // Used for hardware & software SPI
 #define LIS3DH_CS 10
 
-// software SPI
-//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS, LIS3DH_MOSI, LIS3DH_MISO, LIS3DH_CLK);
-// hardware SPI
-//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS);
-// I2C
 Adafruit_LIS3DH lis = Adafruit_LIS3DH();
 
 #if defined(ARDUINO_ARCH_SAMD)
 // for Zero, output on USB Serial console, remove line below if using programming port to program the Zero!
    #define Serial SerialUSB
 #endif
-
-
-void setup21(void) {
-#ifndef ESP8266
-  while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
-#endif
-
-  Serial.begin(9600);
-  Serial.println("LIS3DH test!");
-
-  if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
-    Serial.println("Couldnt start");
-    while (1);
-  }
-  Serial.println("LIS3DH found!");
-
-  lis.setRange(LIS3DH_RANGE_8_G);   // 2, 4, 8 or 16 G!
-
-  Serial.print("Range = "); Serial.print(2 << lis.getRange());
-  Serial.println("G");
-
-  window_clear();
-  daccel_clear();
-}
-
-
 
 void print_data_point(int n) {
   for(int i=0; i<n; i++)
@@ -71,38 +39,6 @@ void print_data_point(int n) {
 // TODO: create a mode that measures this for us
 #define STILL 6.5 //bit of dead area
 
-void loop21() {
-  static unsigned long last_time=0;
-  static unsigned long now=0;
-  static int count=0;
-
-  sensors_event_t event;
-  lis.getEvent(&event);
-
-  window_move_down();
-  window_set_end(event.acceleration.z);
-
-  float amt = window_average()-STILL;
-  if(amt>0.0) {
-    daccel_move_down();
-    daccel_set_end(amt);
-    if(daccel_reversed_direction()) {
-      count++;
-      Serial.print(count);
-      Serial.print(" ");
-      now=millis();
-      double seconds = double(now-last_time)/1000.;
-      Serial.print(1.0/seconds);
-      Serial.println(" Hz");
-      last_time=now;
-    }
-  }
-  else
-    daccel_clear();
-
-  delay(15);
-}
-
 int in1Pin = 4;
 int in2Pin = 5;
 int in3Pin = 6;
@@ -110,52 +46,7 @@ int in4Pin = 7;
 
 Stepper motor(200, in1Pin, in2Pin, in3Pin, in4Pin);
 
-void setup1()
-{
-  //pinMode(in1Pin, OUTPUT);
-  //pinMode(in2Pin, OUTPUT);
-  //pinMode(in3Pin, OUTPUT);
-  //pinMode(in4Pin, OUTPUT);
-
-  // this line is for Leonardo's, it delays the serial interface
-  // until the terminal window is opened
-  //while (!Serial);
-
-  Serial.begin(9600);
-  motor.setSpeed(60);
-}
-
 #define DELAY 500
-
-
-void move(int n, int d) {
-  unsigned long now= millis();
-  motor.step(n);
-  Serial.println(millis()-now);
-  delay(d-(millis()-now));
-}
-
-float freq=1.81;
-
-//float freq=1.70;
-
-//float freq=2.2;
-float del=500./freq;
-
-void loop1() {
-  /* if (Serial.available()) */
-  /* { */
-  /*   int steps = Serial.parseInt(); */
-  /*   motor.step(steps); */
-  /* } */
-  Serial.println(del);
-  move(50, int(del));
-  Serial.println("reverse");
-  move(-50, int(del));
-  delay(0*2*del);
-}
-
-
 
 // The shield uses the I2C SCL and SDA pins. On classic Arduinos
 // this is Analog 4 and 5 so you can't use those for analogRead() anymore
