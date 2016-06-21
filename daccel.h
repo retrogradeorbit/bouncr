33dd66191476cc0b8444728ce60d5a826b51103d
Crispin Wellington <retrogradeorbit@gmail.com>
2016-06-21 22:54:42 +0800

    prevent double import

--- daccel.h
+++ daccel.h
@@ -1,3 +1,6 @@
+#ifndef _DACCEL_H_
+#define _DACCEL_H_
+
 #define DACCEL_WINDOW_SIZE 3
 
 void daccel_clear();
@@ -5,3 +8,5 @@ void daccel_move_down();
 void daccel_set_end(float data);
 bool daccel_reversed_direction();
 float daccel_max();
+
+#endif
