27c17c1442d4dd451718ff36c7abe66cb8d5e598
Crispin Wellington <retrogradeorbit@gmail.com>
2016-06-21 22:53:39 +0800

    pins definition file

---
+++ pins.h
@@ -0,0 +1,17 @@
+/*
+ * Pin configuration for the attached devices on the arduino
+ * Setup for the prototype which was based on a Leonardo
+ * If you configure your connections differently, adjust this file
+ */
+
+#ifndef _PINS_H_
+#define _PINS_H_
+
+#define STEPPER_MOTOR_PIN1 4
+#define STEPPER_MOTOR_PIN2 5
+#define STEPPER_MOTOR_PIN3 6
+#define STEPPER_MOTOR_PIN4 7
+
+#define STEPPER_MOTOR_STEPS_PER_REV 200
+
+#endif
